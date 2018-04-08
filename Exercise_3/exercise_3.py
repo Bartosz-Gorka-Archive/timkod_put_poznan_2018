cf4bf55ad0fc5543bbb897433aca585db07965da
Bartosz Górka <bartosz.gorka@appunite.com>
2018-04-08 11:27:26 +0200

    Analyze characters

--- Exercise_3/exercise_3.py
+++ Exercise_3/exercise_3.py
@@ -55,44 +55,91 @@ def analyze_content(content):
     return letters
 
 
+def analyze_characters(content, row):
+    # Results - dictionary with letters
+    letters = {}
+
+    # Last characters
+    last_characters = []
+
+    # Loop to iterate
+    for char in content:
+        if len(last_characters) == row:
+            # Fetch dictionary
+            selected_dictionary = letters.get(''.join(last_characters), {})
+
+            # Update total counter
+            total = selected_dictionary.get('--TOTAL--', 0)
+            selected_dictionary.update({'--TOTAL--': total + 1})
+
+            # Update counter this letter
+            cardinality = selected_dictionary.get(char, 0)
+            selected_dictionary.update({char: cardinality + 1})
+
+            # Update selected dictionary
+            letters.update({''.join(last_characters): selected_dictionary})
+
+        # Append char to list
+        last_characters.append(char)
+
+        # Check length - if greater than row -> delete first char (FIFO)
+        if len(last_characters) > row:
+            del (last_characters[0])
+
+    # Return letters dictionary
+    return letters
+
+
 def main():
     debug = True
 
-    file_name = '../Exercise_1/norm_wiki_sample.txt'
+    file_name = 'short_sample.txt'
     content = read_file(file_name)
+
+    value = analyze_characters(content, 1)
+    for key, value in value.items():
+        print(key, value)
 
     #
     # Exercise 1 - Random, all letters with probability 1/37
     #
 
-    letters = list('qwertyuiopasdfghjklzxcvbnm0123456789 ')
-    probability = [1/37 for _ in letters]
-    size = 10_000
-    text = exercise_1_generator(size=size, alphabet=letters, probability=probability)
-    letters_dictionary = analyze_content(text)
-    entropy_result = entropy(letters_dictionary)
-
-    if debug:
-        print('Exercise 1 - Raw, random text')
-        print('\tEntropy =>', entropy_result)
+    # letters = list('qwertyuiopasdfghjklzxcvbnm0123456789 ')
+    # probability = [1/37 for _ in letters]
+    # size = 10_000
+    # text = exercise_1_generator(size=size, alphabet=letters, probability=probability)
+    # letters_dictionary = analyze_content(text)
+    # entropy_result = entropy(letters_dictionary)
+    #
+    # if debug:
+    #     print('Exercise 1 - Raw, random text')
+    #     print('\tEntropy =>', entropy_result)
 
     #
     # Exercise 1 - Analyze content from sample Wiki
     #
 
-    size = 10_000
-    letters_dictionary = analyze_content(content)
-    text = exercise_1_generator(size=size, alphabet=list(letters_dictionary.keys()), probability=list(letters_dictionary.values()))
-    letters_dictionary = analyze_content(text)
-    entropy_result = entropy(letters_dictionary)
-
-    if debug:
-        print('Exercise 1 - Based on text')
-        print('\tEntropy =>', entropy_result)
+    # size = 10_000
+    # letters_dictionary = analyze_content(content)
+    # text = exercise_1_generator(size=size, alphabet=list(letters_dictionary.keys()), probability=list(letters_dictionary.values()))
+    # letters_dictionary = analyze_content(text)
+    # entropy_result = entropy(letters_dictionary)
+    #
+    # if debug:
+    #     print('Exercise 1 - Based on text')
+    #     print('\tEntropy =>', entropy_result)
 
     #
     # Exercise 2
     #
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
+
+    # TODO list
+    # * Analyze entropy for single letter in text
+    # * Analyze entropy for word in text
+    # * Conditional entropy for letters - generic function with row as parameter
+    # * Conditional entropy for word - generic function with row as parameter
+    # * Loop to make ^ calculations for all files
