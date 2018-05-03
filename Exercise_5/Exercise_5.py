0100126853b01c8dbc2d844d6099d5fcb79bdbae
Bartosz Górka <bartosz.gorka@appunite.com>
2018-05-03 11:48:50 +0200

    Create code

--- Exercise_5/Exercise_5.py
+++ Exercise_5/Exercise_5.py
@@ -1,4 +1,22 @@
+import heapq
 import operator
+
+
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __eq__(self, other):
+        return self.freq == other.freq
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+    def __gt__(self, other):
+        return self.freq > other.freq
 
 
 # Read file - return content
@@ -22,7 +40,64 @@ def analyze_content(content):
 
 # Order letters dictionary - top used as first
 def order_dictionary(dictionary):
-    return sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
+    return dict(sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True))
+
+
+# Make heap of nodes
+def make_nodes_heap(dictionary):
+    heap = []
+    for (key, value) in dictionary.items():
+        node = Node(key, value)
+        heapq.heappush(heap, node)
+    return heap
+
+
+# Merge nodes to prepare binary tree
+def merge_nodes(heap):
+    while len(heap) > 1:
+        node1 = heapq.heappop(heap)
+        node2 = heapq.heappop(heap)
+
+        merged = Node(None, node1.freq + node2.freq)
+        merged.left = node1
+        merged.right = node2
+
+        heapq.heappush(heap, merged)
+    return heap
+
+
+# Code helper to prepare dictionary with codes and characters
+def code_helper(root, current_code, codes):
+    # Return when None
+    if not root:
+        return
+
+    # Required assign character to node (only leaf has)
+    if root.char:
+        codes[root.char] = current_code
+        return
+
+    # Recursion
+    code_helper(root.left, current_code + '0', codes)
+    code_helper(root.right, current_code + '1', codes)
+
+
+# Prepare codes
+def make_codes(heap):
+    current_code = ''
+    codes = {}
+    root = heapq.heappop(heap)
+
+    code_helper(root, current_code, codes)
+    return codes
+
+
+# Create code
+def create(dictionary):
+    nodes = make_nodes_heap(dictionary)
+    heap = merge_nodes(nodes)
+    codes = make_codes(heap)
+    return codes
 
 
 # Main function
@@ -32,6 +107,7 @@ def main():
     content = read_file(file_name)
     letters_dictionary, counter = analyze_content(content)
     ordered_dictionary = order_dictionary(letters_dictionary)
+    code = create(ordered_dictionary)
 
 
 if __name__ == '__main__':
