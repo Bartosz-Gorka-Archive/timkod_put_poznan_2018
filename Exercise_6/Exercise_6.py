0c223cc91e7c3fdf0eb39edf403ae57ce3ff1481
Bartosz Górka <bartosz.gorka@appunite.com>
2018-06-01 10:27:28 +0200

    Basic LZW - create basic characters codes

--- Exercise_6/Exercise_6.py
+++ Exercise_6/Exercise_6.py
@@ -19,6 +19,19 @@ def sort_dictionary_items(dictionary):
     return dict(sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True))
 
 
+class BasicLZW:
+    def __init__(self, content, characters):
+        self.content = content
+        self.characters_dictionary = characters
+
+    def create(self):
+        dictionary_with_codes = {}
+        for index, key in enumerate(self.characters_dictionary.keys()):
+            dictionary_with_codes.update({key: index + 1})
+
+        return dictionary_with_codes
+
+
 def main():
     file_name = '../Exercise_3/short_sample.txt'
 
@@ -28,7 +41,9 @@ def main():
     content = read_file(file_name, permissions='r')
     characters_dictionary = analyze_content(content)
     sorted_characters_dictionary = sort_dictionary_items(characters_dictionary)
-    print(sorted_characters_dictionary)
+    basic_lzw = BasicLZW(content, sorted_characters_dictionary)
+    lzw_basic_code = basic_lzw.create()
+    print(lzw_basic_code)
 
 
 if __name__ == '__main__':
