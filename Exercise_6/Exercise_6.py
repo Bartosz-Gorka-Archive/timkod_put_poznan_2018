ebe9dc81bef69b72d271d02c9387537ccee3e008
Bartosz Górka <bartosz.gorka@appunite.com>
2018-06-01 16:43:41 +0200

    Use bits inside encoded value

--- Exercise_6/Exercise_6.py
+++ Exercise_6/Exercise_6.py
@@ -1,10 +1,10 @@
-import math
+import cv2
 import operator
 from bitarray import bitarray
 
 
-def read_file(file_name, permissions='r'):
-    file = open(file_name, permissions)
+def read_file(file_name):
+    file = open(file_name, 'r')
     return file.read()
 
 
@@ -41,27 +41,75 @@ class BasicLZW:
     def codes_to_bits(codes):
         dict_with_bits_codes = {}
         total_items = len(codes)
-        fill = int(math.log(total_items, 2) + 1)
+        fill = total_items.bit_length()
         for (char, code) in codes.items():
             bits = int2bits(code, fill=fill)
             dict_with_bits_codes.update({char: bitarray(bits).to01()})
 
         return dict_with_bits_codes
 
+    def encode(self, codes):
+        code = []
+        current_key = ''
+        basic_key = ''
+        last_element_num = len(codes)
+        current_bit_length = last_element_num.bit_length()
+
+        for letter in self.content:
+            current_key += letter
+            if current_key in codes:
+                basic_key += letter
+            else:
+                code.append(codes.get(basic_key))
+                last_element_num += 1
+
+                # Bump bits array - max length used
+                if last_element_num.bit_length() > current_bit_length:
+                    current_bit_length += 1
+                    for key, value in codes.items():
+                        codes.update({key: bitarray('0' + value).to01()})
+
+                # Update dictionary
+                codes.update({current_key: bitarray(int2bits(last_element_num, fill=current_bit_length)).to01()})
+                basic_key = letter
+                current_key = letter
+
+        code.append(codes.get(basic_key))
+        return code
+
 
 def main():
     file_name = '../Exercise_3/short_sample.txt'
+    # file_name = 'wiki_sample.txt'
 
     ################################
     ####### Lempel–Ziv–Welch #######
     ################################
-    content = read_file(file_name, permissions='r')
+    # lena = 'lena.bmp'
+    # file = open(lena, 'rb')
+    # test = list(file.read())
+    # test_content = ''
+    # for i in test[0:100]:
+    #     test_content += chr(i)
+
+    content = read_file(file_name)
+    content = content[0:100]
+    # content = test_content
     characters_dictionary = analyze_content(content)
     sorted_characters_dictionary = sort_dictionary_items(characters_dictionary)
     basic_lzw = BasicLZW(content, sorted_characters_dictionary)
     lzw_basic_code = basic_lzw.create()
+    # print(lzw_basic_code)
     bits_codes = basic_lzw.codes_to_bits(lzw_basic_code)
     print(bits_codes)
+
+    encoded_content = basic_lzw.encode(bits_codes)
+    # write(encoded_content)
+    # print(len(test))
+    print(bits_codes)
+    print(encoded_content)
+    print(len(content))
+    print(len(encoded_content))
 
 
 if __name__ == '__main__':
